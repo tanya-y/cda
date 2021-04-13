4db26fc37c0f28ea4b3915240ca750f9ec45ecab
tanya-y <01ptanya@gmail.com>
2021-04-13 16:37:18 +0530

    first commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ df=pd.read_csv('incedenceOfMalaria.csv')
 
 st.title('Communicable Disease Analysis')
 st.dataframe(df)
-st.markdown('')
+st.markdown('git remote add origin https://github.com/tanya-y/cda.git')
 
 sidebar=st.sidebar
 sidebar.header('Choose your action')
